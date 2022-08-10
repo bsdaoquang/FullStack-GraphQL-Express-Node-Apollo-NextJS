101c8754f01d6bfdd0aeb2df73d479008f0295e0
Đào Văn Quang <quangdv@vntt.com.vn>
2022-08-10 16:59:59 +0700

    run and connect server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello world Dao quang')
+require('dotenv').config()
+import 'reflect-metadata'
+import express from 'express'
+import { createConnection } from 'typeorm'
+
+const connectDB = async () => {
+  await createConnection({
+    type: 'postgres',
+    database: 'postgres',
+    username: 'postgres',
+    password: 'admin',
+    logging: true,
+    synchronize: true,
+  })
+
+  const app = express()
+
+  app.listen(4000, () => console.log('Server stated on port 4000'))
+}
+
+connectDB().catch((error) => console.log(error))
