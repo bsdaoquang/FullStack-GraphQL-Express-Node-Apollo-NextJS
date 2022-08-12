5558bfe1e50df2f9a56e6355a3ec6f77501ca591
Đào Văn Quang <quangdv@vntt.com.vn>
2022-08-12 15:55:45 +0700

    get session cookie

--- server/src/resolvers/hello.ts
+++ server/src/resolvers/hello.ts
@@ -1,9 +1,15 @@
-import { Query, Resolver } from 'type-graphql'
+import { Context } from '../types'
+import { Ctx, Query, Resolver } from 'type-graphql'
 
 @Resolver()
 export class HelloResolver {
   @Query((_returns) => String)
-  hello() {
+  hello(
+    //get and show user id request
+    @Ctx() { req }: Context,
+  ) {
+    console.log(req.session.userId)
+
     return 'Hello world'
   }
 }
